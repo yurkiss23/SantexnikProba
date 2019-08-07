d39fd016cb178fbf974d9627dd8ae25a3f29300b
yurkiss <terranet23@gmail.com>
2019-08-07 23:20:00 +0300

    begin santexnik mvc

---
+++ SantexnikProba/Scripts/headMenu.js
@@ -0,0 +1,5 @@
+﻿$(function () {
+    //alert(1);
+    $("#head-menu").load("Content/_headMenu.html");
+    $("[data-toggle='tooltip']").tooltip();
+});
---
+++ SantexnikProba/Scripts/index.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+    let canvasTrouble = $("#canvasTrouble");
+    let context = canvasTrouble.get(0).getContext("2d");
+    canvasTrouble.hide();
+    $("#btnShot").on("click", function () {
+        context.canvas.height = 400;
+        $("#btnShot").hide();
+        $("#sign").hide();
+        canvasTrouble.show();
+        let shot = document.createElement("button");
+        shot.className = "btn btn-lg btn-outline-success btn-block";
+        shot.innerText = "Натисніть";
+        quickCall.appendChild(shot);
+    });
+})
